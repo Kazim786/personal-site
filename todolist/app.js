977d3d6ed63148fc4fc9a7ab00a2f6f4ea25ed45
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-03-07 16:50:46 -0600

    used ejs tags

--- todolist/app.js
+++ todolist/app.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser')
 const app = express();
 const router = express.Router
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
+    res.render('index', )
     // res.sendFile(__dirname, '/index.html')
     var today = new Date();
     // res.send("" + today.getDate()) //This gives number only.
 
     //to get what the day actually is: 
     today = today.getDate()
-
+    
     switch(today){
         case 1:
             today = 'Sunday'
@@ -33,8 +36,21 @@ app.get('/', (req, res) => {
         case 7:
             today = 'Saturday'
             break;
+        default:
+            console.log('Error: current day is equal to ' + today);
     }
-    res.send(`<h1> ${today}</h1>`) //used back ticks because i couldnt use empty string and style at the same time.
+    // res.send(`<h1><i> ${today}</i></h1>`) //used back ticks because i couldnt use empty string and style at the same time.
+    //Now if you wanna send multiple HTML encoded messages how would you do that? res.write
+    // res.write(`<p>Yes it is ${today}!</p>`)
+    // res.write('<h1>I can sleep in</h1>')
+    // res.send();
+    //Although this works, It isnt efficient.
+
+    //efficient way, make EJS file and use res.render
+    res.render('index', {theDay: today})
+    
+
+
 })
 
 
